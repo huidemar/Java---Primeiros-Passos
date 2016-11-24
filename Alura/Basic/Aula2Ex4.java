8895d110d232f82b0b1d714e5a7ffe3885b25137
Huidemar Costa <huidemar@gmail.com>
2016-11-24 21:22:22 -0200

    Create Aula2Ex4.java

---
+++ Alura/Basic/Aula2Ex4.java
@@ -0,0 +1,14 @@
+class Exercicio4{
+	public static void main(String[] args) {
+		
+		int numero=1;
+		int soma=0;
+		while (numero<=1000){
+			System.out.print(numero+", ");
+			soma += numero;			
+			numero++;
+
+		}
+		System.out.println(soma);
+	}
+}
