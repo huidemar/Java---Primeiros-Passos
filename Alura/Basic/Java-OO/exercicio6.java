13102c9b8be3d43be73db7f7312be23c3b76cfaf
Huidemar Costa <huidemar@gmail.com>
2016-12-03 15:36:19 -0200

    Create exercicio6.java

---
+++ Alura/Basic/Java-OO/exercicio6.java
@@ -0,0 +1,22 @@
+class AtualizadorDeContas{
+
+	private double saldoTotal = 0;
+	private double selic;
+
+	public AtualizadorDeContas(double selic){
+		this.selic = selic;
+	}
+
+	public void roda(Conta c){
+		System.out.println("Saldo anterior da conta "+c.getSaldo());
+		
+		c.atualiza(this.selic);
+
+		System.out.println("Saldo atualizado "+c.getSaldo());
+		this.saldoTotal+=c.getSaldo();
+	}
+
+	public double getSaldoTotal(){
+		return this.saldoTotal;
+	}
+}
