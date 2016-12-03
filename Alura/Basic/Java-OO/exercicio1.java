08bc4e549c5eb1f5d35d8a5e8b6fffcb4c82a99a
Huidemar Costa <huidemar@gmail.com>
2016-12-03 15:04:20 -0200

    Create exercicio1.java

---
+++ Alura/Basic/Java-OO/exercicio1.java
@@ -0,0 +1,15 @@
+class Conta{
+	private double saldo;
+
+	public double getSaldo(){
+		return this.saldo
+	}
+
+	public void deposita(double valorASerDepositado){
+		this.saldo +=valorASerDepositado;
+	}
+
+	public void saca(double valoraSerSacado){
+		this.saldo -= valoraSerSacado;
+	}
+}
